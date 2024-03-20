aadc9007eb7438c6338549570dcfb3f6a4a0b904
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-20 12:54:21 +0000

    Ex1 Criadas variáveis e mostra os valores no ecrã, por enquanto em milisegundos

---
+++ Chronos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            crono1.Start();
+            Thread.Sleep(600);
+            crono2.Start();
+            Thread.Sleep(200);
+            crono1.Stop();
+            crono2.Stop();
+            float tempo1 = crono1.ElapsedMilliseconds;
+            Console.Write($"Cronómetro 1: {tempo1}s\n");
+            Console.Write($"Cronómetro 2: {crono2.ElapsedMilliseconds}s");
+
+
+        }
+    }
+}
