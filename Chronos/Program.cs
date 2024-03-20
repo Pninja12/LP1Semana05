dcaddfc7e27b8ec156a440bb7a53df93d41af003
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-20 19:03:53 +0000

    Ex1 programa devidamente comentado

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -6,22 +6,32 @@ namespace Chronos
 {
     class Program
     {
+        /// <summary>
+        /// Programa que faz 2 cronómetros correrem com uma diferença de começo
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //criação dos dois cronómetros
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
+
+            //começo do cronómetro 1
             crono1.Start();
             Thread.Sleep(600);
+
+            //começo do cronómetro 2
             crono2.Start();
             Thread.Sleep(200);
+
+            //paragem dos dois cronómetros
             crono1.Stop();
             crono2.Stop();
+
             float tempo1 = crono1.ElapsedMilliseconds;
             float tempo2 = crono2.ElapsedMilliseconds;
-            Console.Write($"Cronómetro 1: {tempo1/1000:f3}s\n");
-            Console.Write($"Cronómetro 2: {tempo2/1000:f3}s");
-
-
+            Console.WriteLine($"Cronómetro 1: {tempo1/1000:f3}s");
+            Console.WriteLine($"Cronómetro 2: {tempo2/1000:f3}s");
         }
     }
 }
