cf4fa4e0cc6ceb664420facde39146e8ed5ba62d
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-21 10:17:51 +0000

    Ex5 concluído e testado

---
+++ RanDice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random(int.Parse(args[1]));
+            int soma = 0;
+
+            for(int dado = 0;dado < int.Parse(args[0]);dado++)
+            {
+                soma += generator.Next(1, 7);
+            }
+
+            Console.WriteLine(soma);
+
+        }
+    }
+}
