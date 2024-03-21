d64fe22089913a9866f9740dfbe8650dc429cd46
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-21 11:02:26 +0000

    Ex6 criação da classe Enemy

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -4,11 +4,21 @@ namespace RanDice
 {
     class Program
     {
+        /// <summary>
+        /// Programa que receb na linha de comandos dois números, o primeiro que
+        /// indica a quantidade de dados a serem lançados e o segundo indica a 
+        /// 'seed' a que os dados serão lançados
+        /// </summary>
+        /// <param name="args">o 1º é os dados e o 2º é a 'seed'</param>
         static void Main(string[] args)
         {
+            //inicialização do gerador que recebe a 'seed'
             Random generator = new Random(int.Parse(args[1]));
+            
+            //inicialização da variável com a soma dos dados
             int soma = 0;
 
+            //lançamento de dados
             for(int dado = 0;dado < int.Parse(args[0]);dado++)
             {
                 soma += generator.Next(1, 7);
