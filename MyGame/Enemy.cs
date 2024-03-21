30b3134eef9777e92d5aba01d07bd4bfebb69ba8
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-21 11:27:11 +0000

    Ex6 adição do construtor

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -2,5 +2,15 @@
 {
     public class Enemy
     {
+        private string name;
+        private float health;
+        private float shield;
+        // C o n s t r u t o r
+        public Enemy ( string name )
+        {
+            this.name = name ; // Po rque r a z a o usamos " t h i s " ?
+            health = 100;
+            shield = 0;
+        }
     }
 }
