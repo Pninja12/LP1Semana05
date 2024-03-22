ea4a94ea82386ff56a31e8d97fd1131303acfc4c
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-22 13:33:56 +0000

    Ex10 devidamente comentado

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -7,9 +7,10 @@ namespace MyGame
         private string name;
         private float health;
         private float shield;
-        public Enemy (string name)
+        
+        public Enemy ()
         {
-            this.name = name ;
+            name = "";
             health = 100;
             shield = 0;
         }
--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -4,18 +4,26 @@ namespace MyGame
 {
     class Program
     {
+        /// <summary>
+        /// Programa que recebe o número de inimigos e pergunta o nome de cada 
+        /// um para no fim imprimir o nome de cada e os seus atributos
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            ///Cria a lista de inimigos com o tamanho recebido
             Enemy[] ListaInimigos = new Enemy[int.Parse(args[0])];
 
-
+            //Loop para perguntar e atribuir o nome aos inimigos
             for(int i = 0; i < int.Parse(args[0]); i++)
             {
-                Console.Write($"Nome do inimigo {i + 1} : ");
-                ListaInimigos[i] = new Enemy("");
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                //Inicializa cada inimigo dentro da lista
+                ListaInimigos[i] = new Enemy();
                 ListaInimigos[i].SetName(Console.ReadLine());
             }
 
+            //Loop para mostrar os inimigos
             foreach(Enemy Inimigo in ListaInimigos)
             {
                 Console.Write($"{Inimigo.GetName()} ");
